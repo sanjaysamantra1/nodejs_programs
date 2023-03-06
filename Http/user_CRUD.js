12a6cd32685d472947605fb4eb1729fc7377340c
Sanjay Samantra <sanjaysamantra1@gmail.com>
2023-03-06 09:23:56 +0530

    user crud updated

--- Http/user_CRUD.js
+++ Http/user_CRUD.js
@@ -1,32 +1,36 @@
 const http = require("http");
-const fs = require("fs");
+const users = require("./users.json");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
-  const userId = req.url.split("/").pop();
+  const userId = req.url.split("users/").pop();
+
+  console.log(url, method, userId);
 
   if (url === "/") {
     res.write(`<h1>This is the home page</h1>`);
   }
   if (url === "/users") {
-    res.write(`<h1>get All Users</h1>`);
+    res.write(JSON.stringify(users));
   }
-  if (url.includes("/users") && userId) {
-    res.write(`<h1>get data for  User ${userId}</h1>`);
+  if (url.includes("/users/") && userId) {
+    const user = users.find((user) => user.id == userId);
+    res.write(JSON.stringify(user));
   }
   if (url === "/users" && method === "POST") {
     res.write(`<h1>User Created Successfully!!!</h1>`);
   }
-  if (url === "/users" && method === "PUT") {
+  if (url === "/users/" && method === "PUT") {
     res.write(`<h1>User Updated Successfully!!!</h1>`);
   }
-  if (url === "/users" && method === "PATCH") {
+  if (url === "/users/" && method === "PATCH") {
     res.write(`<h1>User Updated Successfully!!!</h1>`);
   }
-  if (url === "/users" && method === "DELETE") {
+  if (url === "/users/" && method === "DELETE") {
     res.write(`<h1>User Deleted Successfully!!!</h1>`);
   }
+  res.end();
 });
 server.listen(5000, () => {
   console.log("server running on port 5000");
