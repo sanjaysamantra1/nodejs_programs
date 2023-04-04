a2ac228810a927abc947e960261ce1df40f1b7d4
Sanjay Samantra <sanjaysamantra1@gmail.com>
2023-04-04 23:53:46 +0530

    removing unwanted files

--- unit_testing/spec/tests/index.spec.js
+++ unit_testing/spec/tests/index.spec.js
@@ -1,7 +1,9 @@
 const { isPrime } = require("../../index");
 
-describe("Testing Index,js file", () => {
-  it("should test the fibonacci()", () => {
+describe("Testing Index.js file", () => {
+  it("should test the isPrime()", () => {
     expect(isPrime(2)).toBe(true);
+    expect(isPrime(3)).toBe(true);
+    expect(isPrime(4)).toBe(true);
   });
 });
