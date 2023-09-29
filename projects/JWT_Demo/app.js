ba62e248c7e625ebb08fd0fe4ab0b1b5b936a34b
Sanjay Samantra <sanjaysamantra1@gmail.com>
2023-09-29 22:05:32 +0530

    jwt

--- projects/JWT_Demo/app.js
+++ projects/JWT_Demo/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 app.use(express.json());
@@ -15,13 +16,51 @@ app.get("/api/auth/users", async function (req, res) {
   res.json(users);
 });
 app.post("/api/auth/register", async function (req, res) {
-  req.body.password  = bcrypt.hashSync(req.body.password, 8);
+  req.body.password = bcrypt.hashSync(req.body.password, 8);
   try {
     const newUser = new userModel(req.body);
     await newUser.save();
     res.send("User Added successfully");
   } catch (err) {
     console.log(err);
+  }
+});
+app.post("/api/auth/login", async function (req, res) {
+  try {
+    let user = await userModel.findOne({ email: req.body.email });
+    if (!user)
+      res.send({ auth: false, msg: "No User Found,Please Register First" });
+    else {
+      const passIsValid = bcrypt.compareSync(req.body.password, user.password);
+
+      if (!passIsValid) res.send({ auth: false, msg: "Invalid Password" });
+
+      // in case both email & password are correct, generate token and send
+      let token = jwt.sign({ id: user._id }, "mySecretCode", {
+        expiresIn: 60 * 2, //  2 mins
+      });
+      res.send({ auth: true, token: token, expiresIn: 60 * 2 });
+    }
+  } catch (err) {
+    res.send({ auth: false, token: "Error while login" });
+  }
+});
+
+// Logout
+app.get("/logout", (req, res) => {
+  res.send("Logged out Successfully");
+});
+
+app.get("/employees", async function (req, res) {
+  let token = req.headers["my-token"];
+  if (!token) res.send({ auth: false, token: "No Token Provided" });
+
+  //jwt verify
+  try {
+    let user = jwt.verify(token, "mySecretCode");
+    res.send({ auth: true, data: "this is the response from server" });
+  } catch (err) {
+    res.send({ auth: false, token: "Invalid Token" });
   }
 });
 
