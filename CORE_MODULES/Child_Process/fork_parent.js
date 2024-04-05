dae098d5a17758b3086a1d5e3f00466268dea273
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-04-05 09:42:28 +0530

    fork

---
+++ CORE_MODULES/Child_Process/fork_parent.js
@@ -0,0 +1,9 @@
+const cp = require('node:child_process');
+const n = cp.fork(`${__dirname}/sub.js`);
+
+n.on('message', (m) => {
+  console.log('PARENT got message:', m);
+});
+
+// Causes the child to print: CHILD got message: { hello: 'world' }
+n.send({ hello: 'world' });
